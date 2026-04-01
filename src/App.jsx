324367862535ed2d0a7e6bbfd07e5f881c3bd439
Microwave2000 <nagyzsadam@gmail.com>
2026-04-01 19:33:00 +0200

    Components separated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import Examples from "./components/Examples.jsx";
-import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
+        <CoreConcepts />
         <Examples />
       </main>
     </>
